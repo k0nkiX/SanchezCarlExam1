5694bc343cbe72ef1d418eea30c73f22d55788e3
k0nkiX <2015081371@ust-ics.mygbiz.com>
2018-09-13 15:37:37 +0800

    Initial commit

---
+++ app/src/main/java/k/ilagan/sanchezcarlexam1/SanchezCarlExam1.java
@@ -0,0 +1,13 @@
+package k.ilagan.sanchezcarlexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SanchezCarlExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+    }
+}
